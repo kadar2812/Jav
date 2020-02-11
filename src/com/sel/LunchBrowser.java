aa8f2de24b01f7265f5e70a6b0da50571ca4c4ec
kadar2812 <abdulkadar2812@gmail.com>
2020-02-11 09:41:29 +0530

    new project

---
+++ src/com/sel/LunchBrowser.java
@@ -0,0 +1,42 @@
+package com.sel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LunchBrowser {
+public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "G:\\eclipse-workspace\\Selenium\\driver\\chromedriver.exe");
+	    ChromeOptions option = new ChromeOptions();
+	    option.addArguments("start-maximized");
+	    option.addArguments("disable-infobars");
+	    option.addArguments("--headless");
+		WebDriver driver = new ChromeDriver(option);
+		String url = "https://www.facebook.com/";
+		
+		// Lunching url
+		driver.get(url);
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		if (url.equals(currentUrl)) {	
+			System.out.println(true);
+		}
+		else
+		{
+			System.out.println(false);
+		}
+		WebElement testName = driver.findElement(By.id("email"));
+		testName.sendKeys("Mani");
+		WebElement textPass = driver.findElement(By.id("pass"));
+		textPass.sendKeys("Kandan");
+		WebElement btn = driver.findElement(By.xpath("//input[@type='submit']"));
+		btn.click();
+		
+	}
+
+}
